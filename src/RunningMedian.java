1775fdb8589420e82e9f9caeaaa468b13604d30b
abhijay94 <abhijay94@gmail.com>
2018-03-27 15:11:34 +0530

    Added faster solution using Priority Queues that passes all test cases

--- src/RunningMedian.java
+++ src/RunningMedian.java
@@ -1,31 +1,68 @@
-import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 import java.util.Scanner;
 
 public class RunningMedian {
+    public static double[] getMedians(int[] arr) {
+        PriorityQueue<Integer> minHalfHeap = new PriorityQueue<Integer>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer a, Integer b) {
+                return -1 * a.compareTo(b);
+            }
+        });
+        PriorityQueue<Integer> maxHalfHeap = new PriorityQueue<>();
+        double[] medians = new double[arr.length];
+
+        for (int i = 0; i < arr.length; i++) {
+            addNumber(arr[i], minHalfHeap, maxHalfHeap);
+            rebalanceHeap(minHalfHeap, maxHalfHeap);
+            medians[i] = getMedian(minHalfHeap, maxHalfHeap);
+        }
+
+        return medians;
+    }
+
+    private static double getMedian(PriorityQueue<Integer> minHalfHeap, PriorityQueue<Integer> maxHalfHeap) {
+        PriorityQueue<Integer> biggerHeap = minHalfHeap.size() > maxHalfHeap.size() ? minHalfHeap : maxHalfHeap;
+        PriorityQueue<Integer> smallerHeap = minHalfHeap.size() > maxHalfHeap.size() ? maxHalfHeap : minHalfHeap;
+        if(biggerHeap.size() == smallerHeap.size()) {
+            return ((double)biggerHeap.peek() + smallerHeap.peek()) / 2;
+        } else {
+            return biggerHeap.peek();
+        }
+    }
+
+    private static void rebalanceHeap(PriorityQueue<Integer> minHalfHeap, PriorityQueue<Integer> maxHalfHeap) {
+        PriorityQueue<Integer> biggerHeap = minHalfHeap.size() > maxHalfHeap.size() ? minHalfHeap : maxHalfHeap;
+        PriorityQueue<Integer> smallerHeap = minHalfHeap.size() > maxHalfHeap.size() ? maxHalfHeap : minHalfHeap;
+        if(biggerHeap.size() - smallerHeap.size() >= 2) {
+            smallerHeap.add(biggerHeap.poll());
+        }
+
+    }
+
+    private static void addNumber(int num, PriorityQueue<Integer> minHalfHeap, PriorityQueue<Integer> maxHalfHeap) {
+        if (minHalfHeap.isEmpty() || num < minHalfHeap.peek()) {
+            minHalfHeap.add(num);
+        } else {
+            maxHalfHeap.add(num);
+        }
+    }
+
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
         int[] a = new int[n];
-        float[] median = new float[n];
-        for(int a_i=0; a_i < n; a_i++){
+        for (int a_i = 0; a_i < n; a_i++) {
             a[a_i] = in.nextInt();
         }
-        //TODO: This algo is timing out. O(n^2) Implement a faster algorithm using two heaps.
-        for (int i =0; i< n; i++) {
-            float[] temp_a = new float[i+1];
-            for (int j = 0; j<=i; j++) {
-                temp_a[j] = a[j];
-            }
-            Arrays.sort(temp_a);
 
-            if (temp_a.length % 2 != 0) {
-                median[i] = temp_a[(temp_a.length/ 2)];
-            } else {
-                median[i] = (temp_a[temp_a.length / 2] + temp_a[(temp_a.length / 2) - 1])/2;
-            }
-        }
-        for (float i : median){
+        double[] medians = getMedians(a);
+
+        for (double i : medians) {
             System.out.println(String.format("%.1f", i));
         }
     }
+
 }
